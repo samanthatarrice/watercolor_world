69cbf6f088c69f045a2d10d695eed7d52ce3b84e
Samantha Tarrice <s.tarrice@gmail.com>
2021-11-17 16:03:59 -0300

    Make canvas responsie with resize event listener and add circles for user to choose brush size

--- script.js
+++ script.js
@@ -8,8 +8,22 @@ const ctx = canvas.getContext('2d');
 //such a cool trick!!! canvas.width was set in css...let's see if it works with the drawings https://newbedev.com/how-to-make-canvas-responsive
 // :( Doesn't seemm to work
 
-canvas.width = 900;
-canvas.height = 500;
+// canvas.width = 900;
+// canvas.height = 500;
+
+window.addEventListener('load', () => {
+  canvas.width = window.innerWidth/1.4;
+  canvas.height = window.innerHeight/1.5;
+  })
+
+window.addEventListener('resize', () => {
+  canvas.width = window.innerWidth/1.4;
+  canvas.height = window.innerHeight/1.5;
+})
+
+
+
+canvas.style.margin = '0 auto';
 
 let userColor = document.querySelector('#color-picker');
 let userBrushSize = document.querySelector('#brush-size');
@@ -22,6 +36,11 @@ function startStroke(e) {
 }
 
 function finishStroke() {
+  isPainting = false;
+  ctx.beginPath();
+}
+
+function resetStroke() {
   isPainting = false;
   ctx.beginPath();
 }
@@ -46,3 +65,5 @@ canvas.addEventListener('mousedown', startStroke);
 canvas.addEventListener('mouseup', finishStroke);
 
 canvas.addEventListener('mousemove', draw);
+
+canvas.addEventListener('mouseout', resetStroke);
